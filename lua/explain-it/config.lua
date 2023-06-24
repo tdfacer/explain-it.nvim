d4d24fdc5cef0b5c17cbc594c256851bf12bbfa5
Trevor Facer <trevorf@ifit.com>
2023-06-24 08:58:46 -0600

    chore(keymaps): Remove default keymaps
    This removes the default keymaps from plugin configuration, and sets
    default keymaps in the minimal test init used for plugin dev.

--- lua/explain-it/config.lua
+++ lua/explain-it/config.lua
@@ -23,29 +23,6 @@ function M.setup(options)
   local system = require "explain-it.system"
   system.make_system_call(string.format("mkdir -p %s", M.options.output_directory))
 
-  local opts = { noremap = true, silent = true }
-  vim.keymap.set("n", "<space>z", require("explain-it").explain_it, opts)
-  vim.keymap.set("v", "<space>z", require("explain-it").explain_it, opts)
-  vim.keymap.set("n", "<space>Z", function()
-    require("explain-it").explain_it { custom_prompt = true }
-  end, opts)
-  -- vim.keymap.set("v", "<space>Z", function() require"explain-it".explain_it_visual({custom_prompt = true}) end, opts)
-  vim.keymap.set("v", "<space>Z", function()
-    require("explain-it").explain_it { custom_prompt = true, is_visual = true }
-  end, opts)
-  vim.keymap.set("n", "<space>x", function()
-    require("explain-it").explain_it { custom_prompt = false, api_type = "chat" }
-  end, opts)
-  vim.keymap.set("v", "<space>x", function()
-    require("explain-it").explain_it { custom_prompt = false, api_type = "chat", is_visual = true }
-  end, opts)
-  vim.keymap.set("n", "<space>X", function()
-    require("explain-it").explain_it { custom_prompt = true, api_type = "chat" }
-  end, opts)
-  vim.keymap.set("v", "<space>X", function()
-    require("explain-it").explain_it { custom_prompt = true, api_type = "chat", is_visual = true }
-  end, opts)
-
   return M.options
 end
 
--- scripts/test_init.lua
+++ scripts/test_init.lua
@@ -20,5 +20,33 @@ for _, dep in ipairs(deps) do
 vim.opt.rtp:append(dep)
 end
 
+vim.opt.termguicolors = true
 vim.cmd("runtime plugin/plenary.vim")
 require("plenary.busted")
+
+require("notify").setup({
+  background_colour = "#000000",
+})
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<space>z", require("explain-it").explain_it, opts)
+vim.keymap.set("v", "<space>z", require("explain-it").explain_it, opts)
+vim.keymap.set("n", "<space>Z", function()
+  require("explain-it").explain_it { custom_prompt = true }
+end, opts)
+-- vim.keymap.set("v", "<space>Z", function() require"explain-it".explain_it_visual({custom_prompt = true}) end, opts)
+vim.keymap.set("v", "<space>Z", function()
+  require("explain-it").explain_it { custom_prompt = true, is_visual = true }
+end, opts)
+vim.keymap.set("n", "<space>x", function()
+  require("explain-it").explain_it { custom_prompt = false, api_type = "chat" }
+end, opts)
+vim.keymap.set("v", "<space>x", function()
+  require("explain-it").explain_it { custom_prompt = false, api_type = "chat", is_visual = true }
+end, opts)
+vim.keymap.set("n", "<space>X", function()
+  require("explain-it").explain_it { custom_prompt = true, api_type = "chat" }
+end, opts)
+vim.keymap.set("v", "<space>X", function()
+  require("explain-it").explain_it { custom_prompt = true, api_type = "chat", is_visual = true }
+end, opts)
