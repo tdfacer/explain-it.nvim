7c46045277e7ad0deed2e455849993585d9f068a
Trevor Facer <tdfacer@gmail.com>
2026-09-08 06:48:47 -0600

    chore: Clean context builder branch

--- lua/morph.lua
+++ lua/morph.lua
@@ -1016,8 +1016,7 @@ function Morph:mount(tree)
     elseif new_type == "string" or new_type == "number" then
       rendered = new_tree
     elseif new_type == "array" then
-      local old_array = (old_type == "array") and old_tree --[[@as morph.Node[]?]]
-        or nil
+      local old_array = (old_type == "array") and old_tree --[[@as morph.Node[]?]] or nil
       --- @diagnostic disable-next-line: need-check-nil
       rendered = reconcile_array(old_array, new_tree --[[@as morph.Node[] ]])
     elseif new_type == "tag" then
